0a23862564822e7d43031addbce4f64e5914a08b
shaunwassell <shaun.wassell@cbtnuggets.com>
2024-10-08 16:33:45 -0400

    Added aliasing

--- cypress/e2e/selecting.cy.js
+++ cypress/e2e/selecting.cy.js
@@ -1,15 +1,19 @@
 describe('Max character input functionality', () => {
     it('displays the appropriate remaining characters count', () => {
       cy.visit('http://localhost:5173/example-3');
-      cy.get('span[data-cy=first-name-chars-left-count]').eq(0).invoke('text').should('equal', '15');
-      cy.get('#first-name-input').type('Hello')
-      cy.get('span[data-cy=first-name-chars-left-count]').invoke('text').should('equal', '10');
-      cy.get('#first-name-input').type('Hello')
-      cy.get('span[data-cy=first-name-chars-left-count]').invoke('text').should('equal', '5');
-      cy.get('#first-name-input').type('Hello')
-      cy.get('span[data-cy=first-name-chars-left-count]').invoke('text').should('equal', '0');
-      cy.get('#first-name-input').type('Hello')
-      cy.get('#first-name-input').should('have.value', 'HelloHelloHello')
-      cy.get('span[data-cy=first-name-chars-left-count]').invoke('text').should('equal', '0');
+
+      cy.get('span[data-cy=first-name-chars-left-count]').invoke('text').as('fnCharsLeft');
+      cy.get('#first-name-input').as('fnInput');
+
+      cy.get('@fnCharsLeft').should('equal', '15');
+      cy.get('@fnInput').type('Hello')
+      cy.get('@fnCharsLeft').should('equal', '10');
+      cy.get('@fnInput').type('Hello')
+      cy.get('@fnCharsLeft').should('equal', '5');
+      cy.get('@fnInput').type('Hello')
+      cy.get('@fnCharsLeft').should('equal', '0');
+      cy.get('@fnInput').type('Hello')
+      cy.get('@fnInput').should('have.value', 'HelloHelloHello')
+      cy.get('@fnCharsLeft').should('equal', '0');
     })
   })
