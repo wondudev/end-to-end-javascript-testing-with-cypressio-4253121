6dfbe4f3a62a9b53449d168ea7b5c4540297cd96
shaunwassell <shaun.wassell@cbtnuggets.com>
2024-10-08 17:21:39 -0400

    Added beforeeach hook

---
+++ cypress/e2e/before-each.cy.js
@@ -0,0 +1,25 @@
+describe('Max character input functionality', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/example-3');
+
+        cy.get('span[data-cy=first-name-chars-left-count]').invoke('text').as('fnCharsLeft');
+        cy.get('#first-name-input').as('fnInput');
+    });
+
+    it('displays the appropriate remaining characters count with 5 characters', () => {
+        cy.get('@fnCharsLeft').should('equal', '15');
+        cy.get('@fnInput').type('Hello')
+        cy.get('@fnCharsLeft').should('equal', '10');
+    })
+
+    it('displays the appropriate remaining characters count with 10 characters', () => {
+        cy.get('@fnInput').type('HelloHello')
+        cy.get('@fnCharsLeft').should('equal', '5');
+    })
+
+    it('displays the appropriate remaining characters count and prevents extra chars when full', () => {
+        cy.get('@fnInput').type('HelloHelloHelloHello')
+        cy.get('@fnInput').should('have.value', 'HelloHelloHello')
+        cy.get('@fnCharsLeft').should('equal', '0');
+    })
+})
