688a123fa7ec5a7d0be1697f2f36becbdfba8392
shaunwassell <shaun.wassell@cbtnuggets.com>
2024-10-10 10:17:00 -0400

    Added other interaction tests

--- cypress/e2e/interactions.cy.js
+++ cypress/e2e/interactions.cy.js
@@ -7,4 +7,25 @@ describe('basic page interactions', () => {
         cy.get('ul[data-cy=box-1-items-list] > li').eq(2).dblclick();
         cy.get('span[data-cy=box-1-selected-name]').invoke('text').should('equal', 'Option Three');
     })
+
+    it('displays the correct number of checked options', () => {
+        cy.get('span[data-cy=box-2-selected-count]').invoke('text').should('equal', '0');
+
+        cy.get('div[data-cy=box-2-checkboxes] input[type=checkbox]').eq(0).check();
+        cy.get('div[data-cy=box-2-checkboxes] input[type=checkbox]').eq(1).check();
+        cy.get('span[data-cy=box-2-selected-count]').invoke('text').should('equal', '2');
+
+        cy.get('div[data-cy=box-2-checkboxes] input[type=checkbox]').eq(1).uncheck();
+        cy.get('span[data-cy=box-2-selected-count]').invoke('text').should('equal', '1');
+    })
+
+    it('displays the currently selected dropdown option', () => {
+        cy.get('select[data-cy=box-3-dropdown]').select('Option Two');
+        cy.get('span[data-cy=box-3-selected-name]').invoke('text').should('equal', 'Option Two');
+    })
+
+    it('sets the header text to the item name when hovered over', () => {
+        cy.get('ul[data-cy=box-4-items-list] > li').eq(2).trigger('mouseover');
+        cy.get('span[data-cy=box-4-selected-name]').invoke('text').should('equal', 'Option Three');
+    })
 })
