39f9500eed98e8e8f321f6d1048074212cbb0cc7
shaunwassell <shaun.wassell@cbtnuggets.com>
2024-10-10 15:14:10 -0400

    Added cy.and

--- cypress/e2e/results.cy.js
+++ cypress/e2e/results.cy.js
@@ -10,7 +10,9 @@ describe('Max character input functionality', () => {
         cy.get('span[data-cy=first-name-chars-left-count]').invoke('text').as('fnCharsLeft');
         cy.get('#first-name-input').as('fnInput');
 
-        cy.get('@fnCharsLeft').should('equal', '15');
+        cy.get('@fnCharsLeft')
+            .should('exist')
+            .and('equal', '15');
         cy.get('@fnInput').type('Hello')
         cy.get('@fnCharsLeft').should('equal', '10');
         cy.get('@fnInput').type('Hello')
